f1a64601913fe74ac678d4cdd7daeccf46fa74f4
ampivirotto <44910695+ampivirotto@users.noreply.github.com>
2023-02-22 11:45:28 -0500

    updateScript

--- script/Addin.py
+++ script/Addin.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import random
 
 
@@ -10,27 +11,32 @@ for i in range(len(NonCall.index)):
     for k in range(row[1],row[2]+1):
         dic[k] = random.choice(['A','T','G','C'])
 
-addin_df = pd.DataFrame(columns=['Chrom','Pos','rsid','Ref','Alt','Qual','Info','Indi'])
+addin_df = pd.DataFrame(columns=['CHROM','POS','ID','REF','ALT','QUAL', 'FILTER', 'INFO', 'FORMAT','Indi'])
 for i in range(len(NonCall.index)):
     row = NonCall.iloc[i]
     for k in range(row[1],row[2]+1):
         ref = dic[k]
-        go = [row[0],k,'-',ref,'N','-','-','./.']
+        go = [str(row[0]),str(k),'.',ref,'.','.','.', '.', '.','./.']
         addin_df.loc[len(addin_df.index)] = go
 
 
 start = NonCall.iloc[0][1]
 get = 0
-with open('/Users/noahpeles/Downloads/VCFTool.vcf', 'r') as file:
+end = NonCall.iloc[len(NonCall)-1][2]
+with open('VCFTool.vcf', 'r') as file:
     for line in file:
         if line[0] != '#':
             LineParts = line.strip().split('\t')
-            if int(LineParts[1]) > start:
-                for i in range(start,NonCall.iloc[get][2]):
-                    new_line = '    '.join(addin_df.loc[addin_df['Pos']==i])
-                    New.write(new_line)
-                start = NonCall.iloc[get][1]
-                get += 1
+            if int(LineParts[1]) > start and int(LineParts[1]) < end:
+                try:
+                    for i in range(start,NonCall.iloc[get][2]):
+                        new_line = '\t'.join(addin_df.loc[addin_df['POS'].astype(int) == i].values.flatten().tolist())
+                        New.write(new_line + '\n')
+                    start = NonCall.iloc[get][1]
+                    get += 1
+                except:
+                    print('error')
             else:
                 New.write(line)
 
+New.close()
