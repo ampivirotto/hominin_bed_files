0e5b9ca7d0bccd7962273ad5a3577cc09247fff9
No-ha63 <tup41475@temple.edu>
2023-02-22 22:40:55 -0500

    renamed:    script/Addin.py -> script/Addin_Git.py

--- script/Addin_Git.py
+++ script/Addin_Git.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import random
+import random #Doing all twice, not putting in when row in 
 
 
 New = open('open.vcf',"w")
@@ -20,23 +20,29 @@ for i in range(len(NonCall.index)):
         addin_df.loc[len(addin_df.index)] = go
 
 
-start = NonCall.iloc[0][1]
-get = 0
+start = int(NonCall.iloc[0][1])
+get = 1
+end_loop = 0
 end = NonCall.iloc[len(NonCall)-1][2]
-with open('VCFTool.vcf', 'r') as file:
+with open('/Users/noahpeles/Coding/Reseach/hominin_bed_files/script/VCFTool.vcf', 'r') as file:
     for line in file:
         if line[0] != '#':
             LineParts = line.strip().split('\t')
-            if int(LineParts[1]) > start and int(LineParts[1]) < end:
-                try:
-                    for i in range(start,NonCall.iloc[get][2]):
+            if int(LineParts[1]) > start:
+                    if int(LineParts[1]) == end:
+                        go_to = int(NonCall.iloc[end_loop][2])
+                    else:
+                        go_to = int(NonCall.iloc[end_loop][2])+1
+                    for i in range(start,go_to):
                         new_line = '\t'.join(addin_df.loc[addin_df['POS'].astype(int) == i].values.flatten().tolist())
-                        New.write(new_line + '\n')
-                    start = NonCall.iloc[get][1]
-                    get += 1
-                except:
-                    print('error')
+                        New.write(new_line +'\n')
+                    New.write(line)
+                    if int(LineParts[1]) != end:
+                        start = int(NonCall.iloc[get][1])
+                        end_loop += 1
+                        get += 1
             else:
                 New.write(line)
 
 New.close()
+
