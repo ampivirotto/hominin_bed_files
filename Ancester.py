926dec4ce5406d4740bd242120b4f44d9b1344d8
No-ha63 <tup41475@temple.edu>
2023-04-19 11:38:29 -0400

    modified:   Ancester.py

--- Ancester.py
+++ Ancester.py
@@ -3,10 +3,10 @@ import numpy as np
 import gzip
 
 vcf = gzip.open('../chr22/chr22_cds_primates.vcf.gz','r')
-Id1000 = open('../ep/1000K.txt','r')
-IdPongo = open('../ep/Pongo.txt','r')
-IdPan = open('../ep/Pan.txt','r')
-IdArch = open('../ep/Archaics.txt','r')
+# Id1000 = open('../ep/1000K.txt','r')
+# IdPongo = open('../ep/Pongo.txt','r')
+# IdPan = open('../ep/Pan.txt','r')
+# IdArch = open('../ep/Archaics.txt','r')
 """
 vcf = gzip.open('chr22_cds_primates.vcf.gz','r')
 Id1000 = open('IDS/1000K.txt','r')
@@ -14,46 +14,67 @@ IdPongo = open('IDS/Pongo.txt','r')
 IdPan = open('IDS/Pan.txt','r')
 IdArch = open('IDS/Archaics.txt','r')
 """
+def makeIndDictionary(line,file):
+    indDict = {}
+    specDict = {}
+
+    df = pd.read_csv(file, sep = '\t', header = None)
+
+    lineSplit = line.split('\t')
+
+    for i in range(9, len(lineSplit)):
+        idName = lineSplit[i]
+        species = list(df[df[0] == idName][1])[0]
+
+        indDict[i] = species
+
+        if species not in specDict:
+            specDict[species] = [i]
+        else:
+            specDict[species].append(i)
+
+    return indDict, specDict
 
 for line in vcf:   #getting the ids add putting in a list
     line = line.decode('ASCII')
     if line[:3] == '#CH':
         idlist = line.split()
+        no,Go = makeIndDictionary(idlist,pass)
         break
 
-Go = {} #empty dictionary to get list of all different ids and species
-ktho = []
-for line in Id1000: #getting ids for 1000k into a dictionary
-    if len(line)>0:
-        Id = line.split()[0]
-        if Id in idlist:
-            ktho.append(idlist.index(Id))
-Go['1000K'] = ktho
+# Go = {} #empty dictionary to get list of all different ids and species
+# ktho = []
+# for line in Id1000: #getting ids for 1000k into a dictionary
+#     if len(line)>0:
+#         Id = line.split()[0]
+#         if Id in idlist:
+#             ktho.append(idlist.index(Id))
+# Go['1000K'] = ktho
 
-Arch = []
-for line in IdArch: #getting ids for archaics and putting into a dictionary
-    if len(line)>0:
-        Id = line.split()[0]
-        if Id in idlist:
-            Arch.append(idlist.index(Id))
-Go['Archaics'] = Arch
+# Arch = []
+# for line in IdArch: #getting ids for archaics and putting into a dictionary
+#     if len(line)>0:
+#         Id = line.split()[0]
+#         if Id in idlist:
+#             Arch.append(idlist.index(Id))
+# Go['Archaics'] = Arch
 
-Pan = []
-for line in IdPan: #getting ids fo pan and putting into dictionary
-    if len(line)>0:
-        Id = line.split()[0]
-        if Id in idlist:
-            Pan.append(idlist.index(Id))
-Go['Pan'] = Pan
+# Pan = []
+# for line in IdPan: #getting ids fo pan and putting into dictionary
+#     if len(line)>0:
+#         Id = line.split()[0]
+#         if Id in idlist:
+#             Pan.append(idlist.index(Id))
+# Go['Pan'] = Pan
 
-Pongo = []
-for line in IdPongo: #getting ids for pongo and putting into dictionary
-    line = line.strip()
-    if len(line)>0:
-        Id = line.split()[0]
-        if Id in idlist:
-            Pongo.append(idlist.index(Id))
-Go['Pongo'] = Pongo
+# Pongo = []
+# for line in IdPongo: #getting ids for pongo and putting into dictionary
+#     line = line.strip()
+#     if len(line)>0:
+#         Id = line.split()[0]
+#         if Id in idlist:
+#             Pongo.append(idlist.index(Id))
+# Go['Pongo'] = Pongo
 
 
 
@@ -64,7 +85,7 @@ totalLines = 77740
 counter = 1 
 for line in vcf:
     line = line.decode('ASCII')
-    if (counter / totalLines * 100) % 5 == 0:
+    if round((counter / totalLines * 100),2) % 5 == 0:
         print(str(counter/totalLines * 100) + "%")
     counter += 1 
     if line[0] != '#':
@@ -99,7 +120,7 @@ for line in vcf:
             if len(use) != tot/2:
                 print("Ids don't match len of list")
                 exit()
-            if alt/tot+ref/tot+missing/tot != 1:
+            if round(alt/tot+ref/tot+missing/tot,3) != 1:
                 print(alt/tot+ref/tot+missing/tot)
                 print('Frequencies does not equal 1')
                 exit()
