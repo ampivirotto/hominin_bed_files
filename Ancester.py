d3209e99a7d99f87aa0be3113400fcc6ecfb6731
No-ha63 <tup41475@temple.edu>
2023-04-23 23:56:51 -0400

    modified:   Ancester.py

--- Ancester.py
+++ Ancester.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 import gzip
 import sys
-#sys.argv[1] #system input for missingness
 
-vcf = gzip.open('chr22_cds_primates.vcf.gz','r')
-file = open('hominin_bed_files/primate_branch_ids.txt','r')
+missing_stat  = sys.argv[1] #system input for missingness
+vcf = sys.argv[2]
+names = sys.argv[3]
+
+vcf = gzip.open(vcf,'r')
+idfile = open(names,'r')
 # Id1000 = open('../ep/1000K.txt','r')
 # IdPongo = open('../ep/Pongo.txt','r')
 # IdPan = open('../ep/Pan.txt','r')
@@ -43,7 +46,7 @@ def makeIndDictionary(line,file):
 for line in vcf:   #getting the ids add putting in a list
     line = line.decode('ASCII')
     if line[:3] == '#CH':
-        no,Go = makeIndDictionary(line,file)
+        no,Go = makeIndDictionary(line,idfile)
         break
 
 # Go = {} #empty dictionary to get list of all different ids and species
@@ -80,58 +83,167 @@ for line in vcf:   #getting the ids add putting in a list
 #             Pongo.append(idlist.index(Id))
 # Go['Pongo'] = Pongo
 
+def makeTXT(vcf,ids,missing):
+    output_df = pd.DataFrame(columns=['Chromosome','Position','Ref','Alt','1000K-Ref','1000K-Alt','1000K-Missing','Pongo-Ref','Pongo-Alt','Pongo-Missing','Pan-Ref','Pan-ALt','Pan-Missing','Archaic-Ref','Archaic-Alt','Archaic-Missing'])
+    #above is a dataframe just to easily organize the data
+    state_df = pd.DataFrame(columns=['Chromosome','Position','Nucleotide']) #Ensembl will be added later
+
+    # totalLines = 77740
+    # counter = 1 
+    for line in vcf:
+        line = line.decode('ASCII')
+        # if round((counter / totalLines * 100),2) % 5 == 0:
+        #     print(str(counter/totalLines * 100) + "%")
+        # counter += 1 
+        if line[0] != '#':
+            add = [] #lest to add to freq file
+            state_add = [] #list to add to state file
+            lst = line.split() #list of values in line
+            chrome = lst[0] #chrom in line
+            pos = lst[1] #position in line
+            nuke_ref = lst[3] #ref in line
+            nuke_alt = lst[4] #alt in line
+            ancest_missing = 0 
+            ancest_tot = 0
+            ancest_alt = 0 # initiate factors for the state file
+            ancest_ref = 0
+            add.extend([chrome,pos,nuke_ref,nuke_alt]) # add values to list for output files
+            state_add.extend([chrome,pos])
+            for species in ids: #go species by species
+                alt = 0
+                ref = 0 #reset all variables after each species
+                tot = 0
+                missing = 0
+                use = ids[species] #get list of ids
+                for index in use: #go id by id for each species
+                    data = lst[index][:3] #data for eact id
+                    if data == '0|0' or data == '0/0':
+                        ref += 2
+                        tot += 2
+                        if species in ['Pan','Pongo','Gorilla']: #puts in data depeending on species
+                            ancest_ref += 2
+                            ancest_tot += 2
+                    elif data == '1|1' or data == '1/1': #record that data
+                        alt += 2
+                        tot += 2
+                        if species in ['Pan','Pongo','Gorilla']:
+                            ancest_alt += 2
+                            ancest_tot += 2
+                    elif data == '0|1' or data == '1|0' or data == '0/1':
+                        ref += 1
+                        alt += 1
+                        tot += 2
+                    else:
+                        missing += 2
+                        tot += 2
+                        if species in ['Pan','Pongo','Gorilla']:
+                            ancest_missing += 2
+                            ancest_tot += 2
+                if len(use) != tot/2: #check 1
+                    print("Ids don't match len of list")
+                    exit()
+                if round(alt/tot+ref/tot+missing/tot,3) != 1: #check 2
+                    print(alt/tot+ref/tot+missing/tot)
+                    print('Frequencies does not equal 1')
+                    exit()
+                add.extend([alt/tot,ref/tot,missing/tot]) #add data to list
+            ancest_missing_freq = ancest_missing/ancest_tot #get freq of missing in pan,pongo,gorilla
+            ancest_alt_freq = ancest_alt/ancest_tot #get freq of alt in pan,pongo,gorilla
+            ancest_ref_freq = ancest_ref/ancest_tot #freq of ref in pan,pongo,gorilla
+            if ancest_missing_freq >= missing_stat or (ancest_missing_freq>ancest_alt_freq and ancest_missing_freq > ancest_ref_freq):
+                ancest_state = 'NA' #when the ancestor nucleotide will be missing
+            elif ancest_ref_freq > ancest_missing_freq and ancest_ref_freq > ancest_alt_freq:
+                ancest_state = nuke_ref #when ancestor nucleotide will be the ref
+            elif ancest_alt_freq > ancest_missing_freq and ancest_alt_freq > ancest_ref_freq:
+                ancest_state = nuke_alt #when the ancestor nucleotide will be alt
+            state_add.append(ancest_state)
+            output_df.loc[len(output_df.index)] = add #add list to dataframe
+            state_df.loc[len(state_df.index)] = state_add #add list to dataframe for ancestor state
+    output_df.to_csv('Freq.txt',sep='\t',index=False) #convert dataframe to a text file
+    state_df.to_csv('State.txt',sep='\t',index=False) #convert data frame to text file
+
+makeTXT(vcf,Go,missing_stat)
 
 
-output_df = pd.DataFrame(columns=['Chromosome','Position','Ref','Alt','1000K-Ref','1000K-Alt','1000K-Missing','Pongo-Ref','Pongo-Alt','Pongo-Missing','Pan-Ref','Pan-ALt','Pan-Missing','Archaic-Ref','Archaic-Alt','Archaic-Missing'])
-#above is a dataframe just to easily organize the data
+# output_df = pd.DataFrame(columns=['Chromosome','Position','Ref','Alt','1000K-Ref','1000K-Alt','1000K-Missing','Pongo-Ref','Pongo-Alt','Pongo-Missing','Pan-Ref','Pan-ALt','Pan-Missing','Archaic-Ref','Archaic-Alt','Archaic-Missing'])
+# #above is a dataframe just to easily organize the data
+# state_df = pd.DataFrame(columns=['Chromosome','Position','Nucleotide']) #Ensembl will be added later
 
-totalLines = 77740
-counter = 1 
-for line in vcf:
-    line = line.decode('ASCII')
-    if round((counter / totalLines * 100),2) % 5 == 0:
-        print(str(counter/totalLines * 100) + "%")
-    counter += 1 
-    if line[0] != '#':
-        add = []
-        lst = line.split()
-        chrome = lst[0]
-        pos = lst[1]
-        nuke_ref = lst[3]
-        nuke_alt = lst[4]
-        add.extend([chrome,pos,nuke_ref,nuke_alt])   #add tot_missing = 0 could try for total missing
-        for species in Go:
-            alt = 0
-            ref = 0 #reset all variables after each species
-            tot = 0
-            missing = 0
-            use = Go[species] #get list of ids
-            for index in use: #go id by id for each species
-                data = lst[index][:3]
-                if data == '0|0' or data == '0/0':
-                    ref += 2
-                    tot += 2
-                elif data == '1|1' or data == '1/1': #record that data
-                    alt += 2
-                    tot += 2
-                elif data == '0|1' or data == '1|0' or data == '0/1':
-                    ref += 1
-                    alt += 1
-                    tot += 2
-                else:
-                    missing += 2
-                    tot += 2
-            if len(use) != tot/2:
-                print("Ids don't match len of list")
-                exit()
-            if round(alt/tot+ref/tot+missing/tot,3) != 1:
-                print(alt/tot+ref/tot+missing/tot)
-                print('Frequencies does not equal 1')
-                exit()
-            add.extend([alt/tot,ref/tot,missing/tot]) #add data to list
-    output_df.loc[len(output_df.index)] = add #add list to dataframe
+# totalLines = 77740
+# counter = 1 
+# for line in vcf:
+#     line = line.decode('ASCII')
+#     if round((counter / totalLines * 100),2) % 5 == 0:
+#         print(str(counter/totalLines * 100) + "%")
+#     counter += 1 
+#     if line[0] != '#':
+#         add = []
+#         state_add = []
+#         lst = line.split()
+#         chrome = lst[0]
+#         pos = lst[1]
+#         nuke_ref = lst[3]
+#         nuke_alt = lst[4]
+#         ancest_missing = 0
+#         ancest_tot = 0
+#         ancest_alt = 0
+#         ancest_ref = 0
+#         add.extend([chrome,pos,nuke_ref,nuke_alt])
+#         state_add.extend([chrome,pos])
+#         for species in Go:
+#             alt = 0
+#             ref = 0 #reset all variables after each species
+#             tot = 0
+#             missing = 0
+#             use = Go[species] #get list of ids
+#             for index in use: #go id by id for each species
+#                 data = lst[index][:3]
+#                 if data == '0|0' or data == '0/0':
+#                     ref += 2
+#                     tot += 2
+#                     if species in ['Pan','Pongo','Gorilla']:
+#                         ancest_ref += 2
+#                         ancest_tot += 2
+#                 elif data == '1|1' or data == '1/1': #record that data
+#                     alt += 2
+#                     tot += 2
+#                     if species in ['Pan','Pongo','Gorilla']:
+#                         ancest_alt += 2
+#                         ancest_tot += 2
+#                 elif data == '0|1' or data == '1|0' or data == '0/1':
+#                     ref += 1
+#                     alt += 1
+#                     tot += 2
+#                 else:
+#                     missing += 2
+#                     tot += 2
+#                     if species in ['Pan','Pongo','Gorilla']:
+#                         ancest_missing += 2
+#                         ancest_tot += 2
+#             if len(use) != tot/2: #check 1
+#                 print("Ids don't match len of list")
+#                 exit()
+#             if round(alt/tot+ref/tot+missing/tot,3) != 1: #check 2
+#                 print(alt/tot+ref/tot+missing/tot)
+#                 print('Frequencies does not equal 1')
+#                 exit()
+#             add.extend([alt/tot,ref/tot,missing/tot]) #add data to list
+#         ancest_missing_freq = ancest_missing/ancest_tot
+#         ancest_alt_freq = ancest_alt/ancest_tot
+#         ancest_ref_freq = ancest_ref/ancest_tot
+#         if ancest_missing_freq >= missing_stat or (ancest_missing_freq>ancest_alt_freq and ancest_missing_freq > ancest_ref_freq):
+#             ancest_state = 'NA'
+#         elif ancest_ref_freq > ancest_missing_freq and ancest_ref_freq > ancest_alt_freq:
+#             ancest_state = nuke_ref
+#         elif ancest_alt_freq > ancest_missing_freq and ancest_alt_freq > ancest_ref_freq:
+#             ancest_state = nuke_ref
+#         state_add.append(ancest_state)
+#         output_df.loc[len(output_df.index)] = add #add list to dataframe
+#         state_df.loc[len(state_df.index)] = state_add
 
-#output of textfile for chromosome, position, nucleotide, ensembl (European ancester state)
-#calc most common allele for pongo, pan, gorilla /also get the missing amount
+# #output of textfile for chromosome, position, nucleotide, ensembl (European ancester state)
+# #calc most common allele for pongo, pan, gorilla /also get the missing amount
 
-output_df.to_csv('Freq.txt',sep='\t',index=False) #convert dataframe to a text file
+# output_df.to_csv('Freq.txt',sep='\t',index=False) #convert dataframe to a text file
+
+# state_df.to_csv('State.txt',sep='\t',index=False)
