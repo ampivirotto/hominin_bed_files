3a97c92612cbbcef2069f985296577b239a9f4b1
No-ha63 <tup41475@temple.edu>
2023-04-26 15:49:50 -0400

    modified:   AncesterFinal.py

--- AncesterFinal.py
+++ AncesterFinal.py
@@ -45,123 +45,116 @@ def makeTXT(vcf,ids,missing):
     for species in ids.keys():
         for x in ['ref', 'alt', 'alt2', 'alt3', 'missing']:
             cols.append(species + "_" + x)
-    output_df = pd.DataFrame(columns=cols)
+    output_df_dic = {}
     #above is a dataframe just to easily organize the data
-    state_df = pd.DataFrame(columns=['Chromosome','Position','Nucleotide']) #Ensembl will be added later
+    state_df_dic = {} #Ensembl will be added later
 
+    with open(vcf,'r') as vcf:
+        for line in vcf:
+            line = line.decode('ASCII')
 
-    for line in vcf:
-        line = line.decode('ASCII')
+            if line[0] != '#':
+                add = [] #lest to add to freq file
+                state_add = [] #list to add to state file
+                lst = line.split() #list of values in line
+                chrome = lst[0] #chrom in line
+                pos = lst[1] #position in line
+                nuke_ref = lst[3] #ref in line
+                nuke_alt = lst[4] #alt in line
+                ancest_missing, ancest_tot, ancest_alt, ancest_ref = 0, 0, 0, 0 #initiate for acestors
+                an_alt2, an_alt3 = 0, 0
+                state_add.extend([chrome,pos])
 
-        if line[0] != '#':
-            add = [] #lest to add to freq file
-            state_add = [] #list to add to state file
-            lst = line.split() #list of values in line
-            chrome = lst[0] #chrom in line
-            pos = lst[1] #position in line
-            nuke_ref = lst[3] #ref in line
-            nuke_alt = lst[4] #alt in line
-            ancest_missing, ancest_tot, ancest_alt, ancest_ref = 0, 0, 0, 0 #initiate for acestors
-            an_alt2, an_alt3 = 0, 0
-            state_add.extend([chrome,pos])
+                ## figure out how many alt alleles there are
+                triallelic = False
+                quadallelic = False
+                if len(nuke_alt.split(',')) == 2:
+                    triallelic = True
+                    altBP1, altBP2 = nuke_alt.split(',')
+                elif len(nuke_alt.split(',')) == 3:
+                    quadallelic = True
+                    altBP1, altBP2, altBP3 = nuke_alt.split(',')
 
-            ## figure out how many alt alleles there are
-            triallelic = False
-            quadallelic = False
-            if len(nuke_alt.split(',')) == 2:
-                triallelic = True
-                altBP1, altBP2 = nuke_alt.split(',')
-            elif len(nuke_alt.split(',')) == 3:
-                quadallelic = True
-                altBP1, altBP2, altBP3 = nuke_alt.split(',')
+                if triallelic:
+                    add.extend([chrome,pos,nuke_ref,altBP1, altBP2, np.nan]) # add values to list for output files
+                elif quadallelic:
+                    add.extend([chrome,pos,nuke_ref,altBP1, altBP2, altBP3])
+                else:
+                    add.extend([chrome,pos,nuke_ref, nuke_alt, np.nan, np.nan])
 
-            if triallelic:
-                add.extend([chrome,pos,nuke_ref,altBP1, altBP2, np.nan]) # add values to list for output files
-            elif quadallelic:
-                add.extend([chrome,pos,nuke_ref,altBP1, altBP2, altBP3])
-            else:
-                add.extend([chrome,pos,nuke_ref, nuke_alt, np.nan, np.nan])
+                for species in ids: #go species by species
+                    alt = 0
+                    ref = 0 #reset all variables after each species
+                    tot = 0
+                    missing = 0
+                    alt2, alt3 = 0, 0
 
-            for species in ids: #go species by species
-                alt = 0
-                ref = 0 #reset all variables after each species
-                tot = 0
-                missing = 0
-                alt2, alt3 = 0, 0
-
-                use = ids[species] #get list of ids
-                ## iterates through each species
-                for index in use: #go id by id for each species
-                    data = lst[index][:3] #data for eact id
-                    if len(data) < 2:
-                        missing += 2
-                    else:
-                        ref += data.count('0')
-                        alt += data.count('1')
-                        missing += data.count('.')
-                        if triallelic:
-                            alt2 += data.count('2') #data for all species
-                        if quadallelic:
-                            alt2 += data.count('2')
-                            alt3 +=  data.count('3')
-                    tot += 2
-
-                    if species in ['Pan','Pongo','Gorilla']: #puts in data depending on species (just ancestors)
+                    use = ids[species] #get list of ids
+                    ## iterates through each species
+                    for index in use: #go id by id for each species
+                        data = lst[index][:3] #data for eact id
                         if len(data) < 2:
-                            ancest_missing += 2
+                            missing += 2
                         else:
-                            ancest_ref += data.count('0')
-                            ancest_alt += data.count('1')
-                            ancest_missing += data.count('.')
+                            ref += data.count('0')
+                            alt += data.count('1')
+                            missing += data.count('.')
                             if triallelic:
-                                an_alt2 += data.count('2')
+                                alt2 += data.count('2') #data for all species
                             if quadallelic:
-                                an_alt2 += data.count('2')
-                                an_alt3 +=  data.count('3')
-                        ancest_tot += 2
+                                alt2 += data.count('2')
+                                alt3 +=  data.count('3')
+                        tot += 2
 
-                if not triallelic:
-                    alt2 = 0
-                if not quadallelic:
-                    alt3 = 0
+                        if species in ['Pan','Pongo','Gorilla']: #puts in data depending on species (just ancestors)
+                            if len(data) < 2:
+                                ancest_missing += 2
+                            else:
+                                ancest_ref += data.count('0')
+                                ancest_alt += data.count('1')
+                                ancest_missing += data.count('.')
+                                if triallelic:
+                                    an_alt2 += data.count('2')
+                                if quadallelic:
+                                    an_alt2 += data.count('2')
+                                    an_alt3 +=  data.count('3')
+                            ancest_tot += 2
 
-                if len(use) != tot/2: #check 1
-                    print("Ids don't match len of list")
-                    exit()
-                if round(alt/tot+ref/tot+missing/tot+alt2/tot+alt3/tot,3) != 1: #check 2
-                    print(chrome, pos, alt/tot+ref/tot+missing/tot+alt2/tot+alt3/tot)
-                    print('Frequencies does not equal 1')
-                    exit()
 
-                add.extend([ref/tot, alt/tot, alt2/tot, alt3/tot, missing/tot]) #add data to list
+                    if len(use) != tot/2: #check 1
+                        print("Ids don't match len of list")
+                        exit()
+                    if round(alt/tot+ref/tot+missing/tot+alt2/tot+alt3/tot,3) != 1: #check 2
+                        print(chrome, pos, alt/tot+ref/tot+missing/tot+alt2/tot+alt3/tot)
+                        print('Frequencies does not equal 1')
+                        exit()
 
-            if not triallelic:
-                an_alt2 = 0
-            if not quadallelic:
-                an_alt3 = 0
+                    add.extend([ref/tot, alt/tot, alt2/tot, alt3/tot, missing/tot]) #add data to list
 
-            ancest_missing_freq = ancest_missing/ancest_tot #get freq of missing in pan,pongo,gorilla
-            freqs = [ancest_ref/ancest_tot, ancest_alt/ancest_tot, an_alt2/ancest_tot, an_alt3/ancest_tot, ancest_missing_freq]
-            maxFreq = max(freqs)
-            maxIndex = freqs.index(maxFreq)
 
-            if ancest_missing_freq >= missing_stat or (maxIndex == 4):
-                ancest_state = 'NA' #when the ancestor nucleotide will be missing
-            elif maxIndex == 0:
-                ancest_state = nuke_ref #when ancestor nucleotide will be the ref
-            elif maxIndex == 1:
-                if triallelic or quadallelic:
-                    ancest_state = altBP1
-                ancest_state = nuke_alt #when the ancestor nucleotide will be alt
-            elif maxIndex == 2:
-                ancest_state = altBP2
-            elif maxIndex == 3:
-                ancest_state = altBP3
-            state_add.append(ancest_state)
-            output_df.loc[len(output_df.index)] = add #add list to dataframe
-            state_df.loc[len(state_df.index)] = state_add #add list to dataframe for ancestor state
-    output_df.to_csv('Freq.txt',sep='\t',index=False) #convert dataframe to a text file
-    state_df.to_csv('State.txt',sep='\t',index=False) #convert data frame to text file
+
+                ancest_missing_freq = ancest_missing/ancest_tot #get freq of missing in pan,pongo,gorilla
+                freqs = [ancest_ref/ancest_tot, ancest_alt/ancest_tot, an_alt2/ancest_tot, an_alt3/ancest_tot, ancest_missing_freq]
+                maxFreq = max(freqs)
+                maxIndex = freqs.index(maxFreq)
+
+                if ancest_missing_freq >= missing_stat or (maxIndex == 4):
+                    ancest_state = 'NA' #when the ancestor nucleotide will be missing
+                elif maxIndex == 0:
+                    ancest_state = nuke_ref #when ancestor nucleotide will be the ref
+                elif maxIndex == 1:
+                    if triallelic or quadallelic:
+                        ancest_state = altBP1
+                    ancest_state = nuke_alt #when the ancestor nucleotide will be alt
+                elif maxIndex == 2:
+                    ancest_state = altBP2
+                elif maxIndex == 3:
+                    ancest_state = altBP3
+                state_add.append(ancest_state)
+                output_df_dic[len(output_df_dic)] = add #add list to dataframe
+                state_df_dic[len(state_df_dic)] = state_add #add list to dataframe for ancestor state
+    pd.DataFrame.from_dict(output_df_dic, orient='index', columns=cols).to_csv('/Frequencies/Frequencies.csv',index=False)  #convert dataframe to a text file
+    pd.DataFrame.from_dict(state_df_dic,orient='index',columns=['Chromosome','Position','Nucleotide']).to_csv('/State/State.csv',index=False) #convert data frame to text file
 
 makeTXT(vcf,Go,missing_stat)
 
