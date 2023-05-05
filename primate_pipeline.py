4262a8cabc2bb0d2a38a8d8089f2f3182c410eb6
No-ha63 <tup41475@temple.edu>
2023-05-04 20:27:56 -0400

    modified:   primate_pipeline.py

--- primate_pipeline.py
+++ primate_pipeline.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+import re
+import sys
 #-------------------------------------------------------------------------------
 # Name:        module1
 # Purpose:
@@ -8,15 +12,36 @@
 # Copyright:   (c) tuk32868 2023
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+def getChrome(file):
+    to_search = r'chr\d+'
+    all = re.search(to_search,file)
+    if all == None:
+        return 'all'
+    return all[3:]
 
-def VCFfilter():
+def VCFfilter(folder,file,chrome):
+    if chrome == 'all':
+        coding_region_file = './bedfiles/EP_CDS_no_CHR.merged.bed'
+    else:
+        coding_region_file = './bedfiles/'+ chrome +'.merged.bed'
+    title = file.split('.')
+    go = title.index('.vcf')
+    title[go-1] = title[go-1] + '_filtered'
+    output =  folder + '/' + '.'.join(title)
+    command  = 'bcf view -R '+ coding_region_file +' ' + file + ''' -i 'TYPE = "snp"' | bcftools sort -o ''' + output + ' -Oz'
     ##  bcftools view -R (coding region file) (original VCF file of Gorilla/Archaic) -i 'TYPE="snp"' | bcftools sort -o (name of new output VCF) -Oz
+    subprocess.run(command)
+    return output
 
-def index():
+def index(file):
     ## index using bcftools
+    command = 'bcftools index ' + file
+    subprocess.run(command)
 
-def zipUP():
+def zipUP(file):
     ## bgzip using bcftools
+    command = 'bgzip ' + file
+    subprocess.run(command)
 
 def main(location):
     ## split the large single wgs file into chromosomes (gorilla and pongo)
@@ -25,8 +50,10 @@ def main(location):
     for file in os.listdir(location):
         if file.endswith('.vcf.gz'):
             ## filter out snps and sort the files
-            VCFfilter()
-
+            chromosome = getChrome(file)
+            done = VCFfilter(location,file,chromosome)
+            index(done)
+            zipUP(done)
             if not archaic:
                 addNonCall()
             else:
