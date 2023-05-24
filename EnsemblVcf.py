cad5ee55a1d193043aee3723c0a7f9157b6436bb
No-ha63 <tup41475@temple.edu>
2023-05-24 14:17:00 -0400

    new file:   EnsemblVcf.py 	modified:   VCF.py

--- EnsemblVcf.py
+++ EnsemblVcf.py
@@ -1,10 +1,10 @@
 import sys
 import pysam
 
+
 chromosome = sys.argv[1] #only input will be the number of a chomosome
 
 Ensembl_path = './../ensembl/homo_sapiens_ancestor_GRCh37_e71/homo_sapiens_ancestor_{}.fa'.format(chromosome)
-State_path = './States/chrome_{}.csv'.format(chromosome)
 bedfile = './bedfile/{}.merged.bed'.format(chromosome)
 
 def Ensembl(filePath):
@@ -26,16 +26,6 @@ def Ensembl(filePath):
             print(int(end)-int(pos-1))
     return dfdict
 
-def StateDictionary(file2Path):
-    State = {}
-    with open(file2Path,'r') as data:
-        for line in data:
-            lis = line.strip('\n').split(',')
-            if lis[1][0].isnumeric():
-                Position = int(lis[1])
-                State[Position] = lis[2]
-    return State
-
 def REF(chrnum,bedfile):
     """
     get reference genome
@@ -52,12 +42,11 @@ def REF(chrnum,bedfile):
 
 
 
-def MakeVCF(bed,Epath,Spath,chromosome):
+def MakeVCF(bed,Epath,chromosome):
     '''Make VCF file for the state and ensembl data, take in the bed file for iteration
     and paths for the various different files'''
 
     Edic = Ensembl(Epath)
-    Sdic = StateDictionary(Spath)
     Rdic = REF(chromosome,bed)
     output1 = open('Ensembl{}.vcf'.format(chromosome),'w')
     output1.write('##fileformat=VCFv4.2'+'\n')
@@ -65,23 +54,16 @@ def MakeVCF(bed,Epath,Spath,chromosome):
     headers1 = '\t'.join(['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','ENSEMBL'])
     output1.write(headers1+'\n')
 
-    output2 = open('Ancestor_chr{}.vcf'.format(chromosome),'w')
-    output2.write('##fileformat=VCFv4.2'+'\n')
-    output2.write('##Database=PrimateIndividuals'+'\n')
-    headers2 = '\t'.join(['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','PRIMATE'])
-    output2.write(headers2+'\n')
-
 
     with open(bed,'r') as file: #could interate through the merged bed file, and go through those ranges
         for line in file:
                 lis = line.strip('\n').split('\t')
                 start,end = int(lis[1]),int(lis[2])
                 for position in range(start,end+1):
-                    Ensembl_Add,Ancestor_Add = [chromosome],[chromosome]
+                    Ensembl_Add = [chromosome]
                     Ref = Rdic[position]
                     Ensembl_Add += [str(position),'.',Ref]
-                    Ancestor_Add += [str(position),'.',Ref]
-                    Ensembl_nuke,Ancestor_nuke = Edic.get(position,'NA'),Sdic.get(position,'NA')
+                    Ensembl_nuke = Edic.get(position,'NA')
                     if Ensembl_nuke == Ref:
                         Ensembl_Add += ['.','.','.','.','.','0|0']
                     elif Ensembl_nuke == 'NA':
@@ -91,23 +73,8 @@ def MakeVCF(bed,Epath,Spath,chromosome):
                             Ensembl_Add += [Ensembl_nuke,'.','.','.','.','1|1']
                         elif Ensembl_nuke == '.':
                             Ensembl_Add += ['.','.','.','.','.','.']
-                    if Ancestor_nuke == Ref:
-                        Ancestor_Add += ['.','.','.','.','.','0|0']
-                    elif Ancestor_nuke == 'NA':
-                       Ancestor_Add += ['.','.','.','.','.','.|.'] 
-                    else:
-                        if Ancestor_nuke.isalpha():
-                            Ancestor_Add += [Ancestor_nuke,'.','.','.','.','1|1']
-                        elif Ancestor_nuke == '.':
-                            Ancestor_Add += ['.','.','.','.','.','.']
                     Ensembl_String = '\t'.join(Ensembl_Add)
-                    Ancestor_String = '\t'.join(Ancestor_Add)
                     output1.write(Ensembl_String+'\n')
-                    output2.write(Ancestor_String+'\n')
     output1.close()
-    output2.close()
 
-MakeVCF(bedfile,Ensembl_path,State_path,chromosome)
-
-
-#look into merge, option where the sites not in vcf files become reference
+MakeVCF(bedfile,Ensembl_path,chromosome)
